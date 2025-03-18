b0c3e1a29a73b53bb4f46cb14aadcc9c50b27a92
quinng05 <quinng05@vt.edu>
2025-03-18 13:26:46 -0400

    Updated Selenium Test Script

--- scraper.py
+++ scraper.py
@@ -1,2 +1,12 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get('https://play.getfliff.com/')
+
+time.sleep(10)  # Keeps the browser open for 10 seconds
+
+driver.quit()
 
